d8b3d445846b980f4610984aff6b7feede541fbf
ken-b2024 <kenneth.brown.2404@gmail.com>
2025-05-27 16:10:20 -0400

    Upload workflow Github actions

---
+++ config.py
@@ -0,0 +1,14 @@
+
+class DevelopmentConfig:
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:MySQL1sLif3!@localhost/mechanic_shop_db'
+    DEBUG = True
+
+
+class TestingConfig:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///testing.db'
+    DEBUG = True
+    CACHE_TYPE = 'SimpleCache'
+
+
+class ProductionConfig:
+    pass
